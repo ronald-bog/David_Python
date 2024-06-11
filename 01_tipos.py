863fb8459faac3f113be9416c880906d0e966e4a
Ronald Espitia <ronald79@gmail.com>
2024-06-11 17:04:54 -0500

    Clase junio 11

---
+++ 01_tipos.py
@@ -0,0 +1,79 @@
+# VARIABLES
+
+# mi nombre : no dejar espacios
+# mi-nombre : no debe tener gion medio
+# 1nombre : no debe llevar numeros al comienzo
+# if :no puedo utilizar palabras reservadas del lenguaje (Python)
+
+# camelCase = miNombre
+# snake_case = mi_nombre
+
+# sintaxis de las variables: nombre = valor
+
+# Python tipado dinamico
+
+# Declarar
+# Asignar
+# Inicializar
+
+miNumero = 6
+
+# Variable sin valor inicial "declarada"
+nombre = None
+
+# Inferencia de tipos
+
+
+# TIPOS DE DATOS PRIMITIVOS
+
+# NUMERICOS
+
+# Enteros = int Integer
+notaParcial = 4
+cantidadPrendas = 10
+materias_inscritas = 5
+alumnos = 30
+
+alumnos = 20
+
+print(alumnos)
+print(notaParcial)
+
+# Float
+examen = 4.8
+estatura = 1.9
+
+print(estatura)
+
+# STRINGS "Cadenas de caracteres", comillas dobles o simples
+
+fruta = "banano"
+fruta2 = "fresa"
+nombreAlumno = "David Silva"
+cualquierCosa = "ilsdfyhiashfd;hgflsop"
+multilinea = """Hola
+'como'
+estas
+"""
+# animal = "mi perro se llama \"toby\""
+animal2 = 'mi perro se llama "toby"'
+
+print(fruta)
+print(nombreAlumno)
+print(multilinea)
+# print(animal)
+print(animal2)
+
+# TIPO BOOLEANOS True False
+
+correcto = True
+asistencia = False
+
+print(correcto)
+
+print(type(materias_inscritas))
+print(type(examen))
+print(type(fruta))
+print(type(asistencia))
+
+
