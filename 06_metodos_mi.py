c28ce54550c920517495dc7c276c4927cc709fea
Ronald Espitia <ronald79@gmail.com>
2024-06-20 16:06:59 -0500

    Clase junio 20

---
+++ 06_metodos_mi.py
@@ -0,0 +1,57 @@
+# Metodos numeros
+
+# abs()
+nume = -5
+print(abs(nume))
+
+# round
+print(round(3.3))
+print(round(3.14139, 2))
+
+# pow
+print(pow(2, 3))
+
+# divmod
+print(divmod(10, 3))
+
+# max()
+print(max(1, 5, 9, 4))
+
+# min
+print(min(1, 5, 9, 4))
+
+# sum
+print(sum([10, 50, 60]))
+
+# Modulo Math
+import math
+
+# math.sqrt()
+print(math.sqrt(16))
+
+# math.ceil()
+print(math.ceil(3.3))
+
+# floor()
+print(math.floor(3.3))
+print(math.floor(3.9))
+
+# sin(), cos(), tan()
+print(math.sin(math.pi / 2))
+print(math.cos(0))
+print(math.tan(math.pi / 4))
+
+# math.factorial
+print(math.factorial(5))
+
+# math.log
+print(math.log(10))  # logaritmo natural
+print(math.log(100, 10))  # logaritmo base 10
+
+# exp
+print(math.exp(2))
+
+# degrees, radians
+print(math.degrees(math.pi))
+print(math.degrees(3.14))
+print(math.radians(180))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,16 @@
-oracion = "Python es dinamico"
-print(oracion.find("es"))
+import math
+
+# math.sqrt()
+print(math.sqrt(16))
+
+# math.ceil()
+print(math.ceil(3.3))
+
+# floor()
+print(math.floor(3.3))
+print(math.floor(3.9))
+
+# sin(), cos(), tan()
+print(math.sin(math.pi / 2))
+print(math.cos(0))
+print(math.tan(math.pi / 4))
